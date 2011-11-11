48b5a8e3696646a04ad3b909c78faecde56d5640
Taylor Fausak <taylor@fausak.me>
2011-11-10 23:04:11 -0600

    Minify CSS and JS.

--- static/scripts/all.js
+++ static/scripts/all.js
@@ -1,13 +1,1 @@
-// Set up Google Analytics.
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-26535645-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
+var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-26535645-1"]);_gaq.push(["_trackPageview"]);(function(){var b=document.createElement("script");b.type="text/javascript";b.async=true;b.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(b,a)})();
