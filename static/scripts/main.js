1d164bf4e95af84786c8605c9e458c1f1809a3e5
Taylor Fausak <taylor@fausak.me>
2012-04-26 22:50:05 -0500

    Delay loading social widgets until the user scrolls.
    This has two benefits: (1) users that just click and go back won't
    have to load any widgets; and (2) the page load time will be faster
    for GoogleBot, since it doesn't scroll.

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,7 +5,7 @@ var _gaq = [
 
 (function () {
     "use strict";
-    var script;
+    var script, loadedSocial;
 
     // Google Analytics
     script = document.createElement('script');
@@ -13,23 +13,30 @@ var _gaq = [
     script.src = (/^....s/.test(location) ? '//ssl' : '//www') + '.google-analytics.com/ga.js';
     document.body.appendChild(script);
 
-    // Facebook JS SDK
-    script = document.createElement('script');
-    script.async = true;
-    script.id = 'facebook-jssdk';
-    script.src = '//connect.facebook.net/en_US/all.js#xfbml=1&appId=133083533456136';
-    document.body.appendChild(script);
+    // Delay loading social widgets until the user scrolls.
+    window.onscroll = function () {
+        if (!loadedSocial) {
+            loadedSocial = true;
 
-    // Google +1 button
-    script = document.createElement('script');
-    script.async = true;
-    script.src = '//apis.google.com/js/plusone.js';
-    document.body.appendChild(script);
+            // Facebook JS SDK
+            script = document.createElement('script');
+            script.async = true;
+            script.id = 'facebook-jssdk';
+            script.src = '//connect.facebook.net/en_US/all.js#xfbml=1&appId=133083533456136';
+            document.body.appendChild(script);
 
-    // Twitter widgets
-    script = document.createElement('script');
-    script.async = true;
-    script.id = 'twitter-wjs';
-    script.src = '//platform.twitter.com/widgets.js';
-    document.body.appendChild(script);
+            // Google +1 button
+            script = document.createElement('script');
+            script.async = true;
+            script.src = '//apis.google.com/js/plusone.js';
+            document.body.appendChild(script);
+
+            // Twitter widgets
+            script = document.createElement('script');
+            script.async = true;
+            script.id = 'twitter-wjs';
+            script.src = '//platform.twitter.com/widgets.js';
+            document.body.appendChild(script);
+        }
+    };
 })();
