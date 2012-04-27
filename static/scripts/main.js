80a54ffcdb55d889a4d7d79fa5db03420d4d0b01
Taylor Fausak <taylor@fausak.me>
2012-04-26 23:27:12 -0500

    Remove "loadedSocial" flag.
    I realized I could just reassign window.onscroll from inside the
    function.

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,7 +5,7 @@ var _gaq = [
 
 (function () {
     "use strict";
-    var script, loadedSocial;
+    var script;
 
     // Google Analytics
     script = document.createElement('script');
@@ -15,28 +15,26 @@ var _gaq = [
 
     // Delay loading social widgets until the user scrolls.
     window.onscroll = function () {
-        if (!loadedSocial) {
-            loadedSocial = true;
+        window.onscroll = null;
 
-            // Facebook JS SDK
-            script = document.createElement('script');
-            script.async = true;
-            script.id = 'facebook-jssdk';
-            script.src = '//connect.facebook.net/en_US/all.js#xfbml=1&appId=133083533456136';
-            document.body.appendChild(script);
+        // Facebook JS SDK
+        script = document.createElement('script');
+        script.async = true;
+        script.id = 'facebook-jssdk';
+        script.src = '//connect.facebook.net/en_US/all.js#xfbml=1&appId=133083533456136';
+        document.body.appendChild(script);
 
-            // Google +1 button
-            script = document.createElement('script');
-            script.async = true;
-            script.src = '//apis.google.com/js/plusone.js';
-            document.body.appendChild(script);
+        // Google +1 button
+        script = document.createElement('script');
+        script.async = true;
+        script.src = '//apis.google.com/js/plusone.js';
+        document.body.appendChild(script);
 
-            // Twitter widgets
-            script = document.createElement('script');
-            script.async = true;
-            script.id = 'twitter-wjs';
-            script.src = '//platform.twitter.com/widgets.js';
-            document.body.appendChild(script);
-        }
+        // Twitter widgets
+        script = document.createElement('script');
+        script.async = true;
+        script.id = 'twitter-wjs';
+        script.src = '//platform.twitter.com/widgets.js';
+        document.body.appendChild(script);
     };
 }());
