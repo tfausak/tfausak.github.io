173174208f3642e6f1db1f0750f395b6ef00cc75
Taylor Fausak <taylor@fausak.me>
2012-09-01 16:06:09 -0500

    Prevent JavaScript errors on the home page.
    I forgot that the script gets loaded on every page, not just the post
    pages.

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -15,42 +15,48 @@ var _gaq = [
 
     // Facebook like widget
     element = document.getElementById('facebook-widget');
-    element.onmouseover = function () {
-        this.onmouseover = null;
-        this.parentNode.removeChild(this);
-        _gaq.push(['_trackEvent', 'Social', 'Facebook', 'Like']);
+    if (element) {
+        element.onmouseover = function () {
+            this.onmouseover = null;
+            this.parentNode.removeChild(this);
+            _gaq.push(['_trackEvent', 'Social', 'Facebook', 'Like']);
 
-        script = document.createElement('script');
-        script.async = true;
-        script.id = 'facebook-jssdk';
-        script.src = '//connect.facebook.net/en_US/all.js#xfbml=1&appId=133083533456136';
-        document.body.appendChild(script);
-    };
+            script = document.createElement('script');
+            script.async = true;
+            script.id = 'facebook-jssdk';
+            script.src = '//connect.facebook.net/en_US/all.js#xfbml=1&appId=133083533456136';
+            document.body.appendChild(script);
+        };
+    }
 
     // Google +1 widget
     element = document.getElementById('google-widget');
-    element.onmouseover = function () {
-        this.onmouseover = null;
-        this.parentNode.removeChild(this);
-        _gaq.push(['_trackEvent', 'Social', 'Google', '+1']);
+    if (element) {
+        element.onmouseover = function () {
+            this.onmouseover = null;
+            this.parentNode.removeChild(this);
+            _gaq.push(['_trackEvent', 'Social', 'Google', '+1']);
 
-        script = document.createElement('script');
-        script.async = true;
-        script.src = '//apis.google.com/js/plusone.js';
-        document.body.appendChild(script);
-    };
+            script = document.createElement('script');
+            script.async = true;
+            script.src = '//apis.google.com/js/plusone.js';
+            document.body.appendChild(script);
+        };
+    }
 
     // Twitter tweet widget
     element = document.getElementById('twitter-widget');
-    element.onmouseover = function () {
-        this.onmouseover = null;
-        this.parentNode.removeChild(this);
-        _gaq.push(['_trackEvent', 'Social', 'Twitter', 'Tweet']);
+    if (element) {
+        element.onmouseover = function () {
+            this.onmouseover = null;
+            this.parentNode.removeChild(this);
+            _gaq.push(['_trackEvent', 'Social', 'Twitter', 'Tweet']);
 
-        script = document.createElement('script');
-        script.async = true;
-        script.id = 'twitter-wjs';
-        script.src = '//platform.twitter.com/widgets.js';
-        document.body.appendChild(script);
-    };
+            script = document.createElement('script');
+            script.async = true;
+            script.id = 'twitter-wjs';
+            script.src = '//platform.twitter.com/widgets.js';
+            document.body.appendChild(script);
+        };
+    }
 }());
