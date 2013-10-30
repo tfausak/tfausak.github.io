b02a0d0e13f448cf4d2efc693f2f60d8c22678dd
Taylor Fausak <taylor@fausak.me>
2013-10-30 15:29:04 -0500

    Re #13; add Gauges tracking code

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,7 +1,7 @@
 var _gaq = [
     ['_setAccount', 'UA-26535645-1'],
     ['_trackPageview']
-];
+], _gauges = window._gauges || [];
 
 (function () {
     'use strict';
@@ -10,7 +10,15 @@ var _gaq = [
     // Google Analytics
     script = document.createElement('script');
     script.async = true;
-    script.src = (/^https/.test(location) ? '//ssl' : '//www') +
+    script.src = (/^https/.test(window.location) ? '//ssl' : '//www') +
         '.google-analytics.com/ga.js';
+    document.body.appendChild(script);
+
+    // Gauges
+    script = document.createElement('script');
+    script.async = true;
+    script.id = 'gauges-tracker';
+    script.src = '//secure.gaug.es/track.js';
+    script.setAttribute('data-site-id', '527149ff108d7b4f0b000055');
     document.body.appendChild(script);
 }());
