3713e645d50abbe3fad440f76d84afbd34cf5072
Taylor Fausak <taylor@fausak.me>
2013-11-26 09:01:45 -0600

    Re #13; add Piwik tracking code

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -14,3 +14,14 @@ var _gaq = [
         '.google-analytics.com/ga.js';
     document.body.appendChild(script);
 }());
+
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik-fausak.rhcloud.com//";
+_paq.push(['setTrackerUrl', u+'piwik.php']);
+_paq.push(['setSiteId', 1]);
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
